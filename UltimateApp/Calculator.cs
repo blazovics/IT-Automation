33b718549e84a3997232d6ca1e9855928ff1172e
Laszlo Blazovics <blazovics.laszlo@aut.bme.hu>
2022-04-28 15:49:46 +0200

    Update source code folder

---
+++ UltimateApp/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UltimateApp
+{
+    enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Modulo
+    }
+
+    class Calculator
+    {
+        private static int Add (int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Subtract (int a, int b)
+        {
+            return a - b;
+        }
+
+        private static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        private static int Modulo(int a, int b)
+        {
+            return a /b;
+        }
+
+        public static int? calculate(int a, int b, Operation o)
+        {
+            switch (o)
+            {
+                case Operation.Add:
+                    return Add(a, b);
+
+                case Operation.Subtract:
+                    return Subtract(a, b);
+
+                case Operation.Multiply:
+                    return Multiply(a, b);
+
+                case Operation.Divide:
+                    return Divide(a, b);
+
+                case Operation.Modulo:
+                    return Divide(a, b);
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ UltimateApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace UltimateApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a, b;
+
+            a = getInputOperands("Give first operand");
+
+            Operation? o = getOperationType("Give operation type {+,-,*,/,%}");
+
+            b = getInputOperands("Give second operand");
+
+            if (a!=null && b!=null && o != null)
+            {
+                Console.WriteLine(a + " " + o.ToString() + " " + b + " = " + Calculator.calculate(a.Value,b.Value,o.Value));
+            }
+            else
+            {
+                Console.WriteLine("Bad input");
+            }
+
+            
+        }
+
+        private static int? getInputOperands(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+
+            if (input != null)
+            {
+                try
+                {
+                    int result = Int32.Parse(input);
+                    return result;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{input}'");
+                }
+            }
+            return null;
+        }
+
+        private static Operation? getOperationType(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+
+            if (input != null)
+            {
+                switch (input)
+                {
+                    case "+":
+                        return Operation.Add;
+                    case "-":
+                        return Operation.Subtract;
+                    case "*":
+                        return Operation.Multiply;
+                    case "/":
+                        return Operation.Divide;
+                    case "%":
+                        return Operation.Modulo;
+                }
+            }
+            return null;
+        }
+    }
+}
